98f9548ac2de8784788ef6259cae3e726ad62bf2
kienn6034 <kien@notional.ventures>
2024-02-22 17:05:20 +0700

    chore: setup relayer

--- relayer/setup/rly-init.sh
+++ relayer/setup/rly-init.sh
@@ -8,9 +8,9 @@ echo ""
 
 # Configure predefined mnemonic pharses
 BINARY=rly
-CHAIN_DIR=$(pwd)/data
-CHAINID_1=test-1
-CHAINID_2=test-2
+CHAIN_DIR=$(pwd)/testnet
+CHAINID_1=cudos
+CHAINID_2=axelar
 RELAYER_DIR=/relayer
 MNEMONIC_1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
 MNEMONIC_2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
@@ -26,8 +26,8 @@ echo "Initializing $BINARY..."
 $BINARY config init --home $CHAIN_DIR/$RELAYER_DIR
 
 echo "Adding configurations for both chains..."
-$BINARY chains add-dir ./scripts/tests/relayer/interchain-acc-config/chains --home $CHAIN_DIR/$RELAYER_DIR
-$BINARY paths add $CHAINID_1 $CHAINID_2 test1-test2 --file ./scripts/tests/relayer/interchain-acc-config/paths/test1-test2.json --home $CHAIN_DIR/$RELAYER_DIR
+$BINARY chains add-dir ./relayer/setup/chains --home $CHAIN_DIR/$RELAYER_DIR
+$BINARY paths add $CHAINID_1 $CHAINID_2 cudos-axelar --file ./relayer/setup/paths/cudos-axelar.json --home $CHAIN_DIR/$RELAYER_DIR
 
 echo "Restoring accounts..."
 $BINARY keys restore $CHAINID_1 testkey "$MNEMONIC_1" --home $CHAIN_DIR/$RELAYER_DIR
